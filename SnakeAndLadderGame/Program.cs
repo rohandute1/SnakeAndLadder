c93ca98ea1c30151ed88fe57c870c0f430d277d8
rohandute1 <132436294+rohandute1@users.noreply.github.com>
2023-05-28 23:27:05 +0530

    Update Program.cs

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -14,21 +14,18 @@ namespace SnakeAndLadderGame
 
             int position = 0;
             int diceRolls = 0;
+            Random random = new Random();
 
             while (position < 100)
             {
-                Console.WriteLine("Press any key to roll the dice...");
-                Console.ReadKey();
-
                 
-                Random random = new Random();
                 int diceValue = random.Next(1, 7);
                 diceRolls++;
 
-                
+            
                 position += diceValue;
 
-                
+            
                 if (position == 4)
                     position = 14; 
                 else if (position == 9)
@@ -62,7 +59,8 @@ namespace SnakeAndLadderGame
                 else if (position == 99)
                     position = 78;
 
-            if (position > 100)
+            
+                if (position > 100)
                     position -= diceValue;
 
                 Console.WriteLine("You rolled a {0}. Your current position is {1}.", diceValue, position);
@@ -72,5 +70,6 @@ namespace SnakeAndLadderGame
             Console.ReadLine();
         }
     }
+
 
 }
