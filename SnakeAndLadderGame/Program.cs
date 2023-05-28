f127468ff0ccd1f9ededc07c43bf08348e573ed5
Rohan <rohandute1@gmail.com>
2023-05-28 23:08:23 +0530

    [Rohan] UC1 solved successfully

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -10,8 +10,67 @@ namespace SnakeAndLadderGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder");
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+
+            int position = 0;
+            int diceRolls = 0;
+
+            while (position < 100)
+            {
+                Console.WriteLine("Press any key to roll the dice...");
+                Console.ReadKey();
+
+                
+                Random random = new Random();
+                int diceValue = random.Next(1, 7);
+                diceRolls++;
+
+                
+                position += diceValue;
+
+                
+                if (position == 4)
+                    position = 14; 
+                else if (position == 9)
+                    position = 31; 
+                else if (position == 17)
+                    position = 7; 
+                else if (position == 20)
+                    position = 38;
+                else if (position == 28)
+                    position = 84;
+                else if (position == 40)
+                    position = 59;
+                else if (position == 51)
+                    position = 67;
+                else if (position == 54)
+                    position = 34;
+                else if (position == 62)
+                    position = 19;
+                else if (position == 63)
+                    position = 81;
+                else if (position == 64)
+                    position = 60;
+                else if (position == 71)
+                    position = 91;
+                else if (position == 87)
+                    position = 24;
+                else if (position == 93)
+                    position = 73;
+                else if (position == 95)
+                    position = 75;
+                else if (position == 99)
+                    position = 78;
+
+            if (position > 100)
+                    position -= diceValue;
+
+                Console.WriteLine("You rolled a {0}. Your current position is {1}.", diceValue, position);
+            }
+
+            Console.WriteLine("Congratulations! You reached 100 in {0} dice rolls.", diceRolls);
             Console.ReadLine();
         }
     }
+
 }
